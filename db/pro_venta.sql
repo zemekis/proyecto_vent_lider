5027ef1a5769c0fc3510c1eedfc4686aa3bc3f5e
LAB-315 <LAB-315@LAB-315-PC>
2016-04-21 13:26:28 -0300

    se cambia el script a m-m en ventas con la tabla compras se agrega el campo cantidad para añadir mas de un producto

--- db/pro_venta.sql
+++ db/pro_venta.sql
@@ -28,8 +28,8 @@ stock_producto int,
 marcaFK int ,
 tipoFK int,
 primary key(id_producto),
-foreign key(marcaFK) references marca(id_marca),
-foreign key(tipoFK) references tipo(id_tipo)
+foreign key(marcaFK) references marca(id_marca) on delete cascade,
+foreign key(tipoFK) references tipo(id_tipo) on delete cascade
 );
 
 create table tipocliente(
@@ -60,12 +60,12 @@ fecha_venta timestamp,
 metodo_pagoFK int,
 id_usuarioFK  int,
 id_clienteFK int ,
-id_productoFK int,
+id_compraFK int,
 primary key (id_venta),
 foreign key (metodo_pagoFK)references metodoPago(id_metodopago),
 foreign key (id_usuarioFK) references usuario(id_usuario),
 foreign key (id_clienteFK)references cliente(id_cliente),
-foreign key (id_productoFK)references producto(id_producto)
+foreign key (id_compraFK)references compra(id_compra)
 );
 
 
@@ -74,6 +74,15 @@ id_registroHistorico int auto_increment,
 accion_registroHistorico varchar(50),
 fecha_registroHistorico timestamp,
 primary key(id_registrohistorico));
+
+
+create table compra(
+id_compra int auto_increment,
+id_productoFK int,
+cantidad int,
+primary key (id_compra),
+foreign key (id_productoFK) references producto(id_producto)
+);
 
 /*
 create table registroventa(
@@ -87,7 +96,7 @@ foreign key (id_ventaFK) references venta(id_venta)
 
 insert into usuario values(null,'usuario1','user12345');
 
--- drop database pro_venta;
+ drop database pro_venta;
 -- drop table cliente;
 -- drop table marca;
 -- drop table metodoPago;
