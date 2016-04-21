28c7daa816145f7675e16b3c95bda35934c500a8
LAB-315 <LAB-315@LAB-315-PC>
2016-04-21 11:56:33 -0300

    se crean los paquetes y las clases completas, solo el dao y conexion estan vacios se cambian letras de el script

--- src/controlador/DAO.java
+++ src/controlador/DAO.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package controlador;
 
 /**
  *
  * @author Zemekis
  */
-public class Conexion {
-   
+public class DAO {
+    
 }
